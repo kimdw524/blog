1561ba0b90e781eeaf7345b939dc43287834c9cb
= <kimdw524@naver.com>
2024-09-09 02:18:22 +0900

    chore: add react-router-dom

--- packages/blog/src/App.tsx
+++ packages/blog/src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import Home from './pages/Home';
+
 function App() {
-  return <div className="App">asd</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

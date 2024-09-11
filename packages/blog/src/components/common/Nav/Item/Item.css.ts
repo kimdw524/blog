97af88bb6a26821e511e89a6b439d3faddcf4f7b
= <kimdw524@naver.com>
2024-09-12 02:35:37 +0900

    design: nav item 디자인

--- packages/blog/src/components/common/Nav/Item/Item.css.ts
+++ packages/blog/src/components/common/Nav/Item/Item.css.ts
@@ -1,6 +1,19 @@
+import { colorPalette, theme } from '@kimdw524/dds';
+
 import { style } from '@vanilla-extract/css';
 
 export const item = style({
+  padding: '0.5rem 0.75rem',
+  borderRadius: '1rem',
+  color: theme.color.text.secondary,
   fontSize: '1rem',
   fontWeight: 'normal',
+  whiteSpace: 'nowrap',
+  transition: 'all 0.3s ease',
+  cursor: 'pointer',
+  userSelect: 'none',
+  ':hover': {
+    backgroundColor: colorPalette.grey[200],
+    color: theme.color.text.primary,
+  },
 });

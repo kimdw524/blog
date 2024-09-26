3c57ff331780745b87e9abee94a728786c381a03
= <kimdw524@naver.com>
2024-09-26 22:55:57 +0900

    feat: Image 컴포넌트 Viewport 안에 있을 때만 로딩

--- packages/blog/src/components/common/Image/index.tsx
+++ packages/blog/src/components/common/Image/index.tsx
@@ -1,6 +1,8 @@
-import { mergeClass } from '@/utils/mergeClass';
+import { useRef, useState } from 'react';
 
-import { useState } from 'react';
+import useIsInViewport from '@/hooks/useIsInViewport';
+
+import { mergeClass } from '@/utils/mergeClass';
 
 import * as s from './Image.css';
 
@@ -19,19 +21,28 @@ type ImageState = 'loading' | 'success' | 'error';
 const Image = ({ src, alt, width, height, className }: ImageProps) => {
   const [state, setState] = useState<ImageState>('loading');
 
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const { isExposed } = useIsInViewport(containerRef);
+
   return (
     <div
+      ref={containerRef}
       className={mergeClass(s.container, className)}
       style={{ width, height }}
     >
-      <img
-        src={src}
-        alt={alt}
-        className={s.image}
-        onLoad={() => setState('success')}
-        onError={() => setState('error')}
-      />
-      <Loading done={state === 'success'} />
+      {isExposed && (
+        <>
+          <img
+            src={src}
+            alt={alt}
+            className={s.image}
+            onLoad={() => setState('success')}
+            onError={() => setState('error')}
+          />
+          <Loading done={state === 'success'} />
+        </>
+      )}
     </div>
   );
 };

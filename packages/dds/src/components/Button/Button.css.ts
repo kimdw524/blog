7412d6f73f081d8853f4acfcbdc8452e3328d0d0
= <kimdw524@naver.com>
2024-10-01 04:21:04 +0900

    design: Button style 수정

--- packages/dds/src/components/Button/Button.css.ts
+++ packages/dds/src/components/Button/Button.css.ts
@@ -4,12 +4,10 @@ import theme, { type Palette } from '../../styles/theme.css';
 
 export const button = recipe({
   base: {
-    padding: '0.75rem',
     border: 0,
     borderRadius: '0.25rem',
     backgroundColor: theme.palette.primary.main,
     color: theme.palette.primary.contrastText,
-    lineHeight: 0,
     cursor: 'pointer',
     transition: 'all 0.1s ease',
     userSelect: 'none',
